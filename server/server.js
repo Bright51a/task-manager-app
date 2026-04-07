63c0e11f6f0c59d9d7381291e95c528d9ee3bdef
b18129805-design <b18129805@gmail.com>
2026-04-07 01:38:55 +0100

    fix cors crash

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ app.use(cors({
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"]
 }));
-app.options("*", cors());
+app.use(cors());
 app.use(express.json());
 
 // Connect to MongoDB
